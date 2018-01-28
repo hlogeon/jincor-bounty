274059377901d42b79f294d99725e003853cab7d
Andrey Degtyaruk <hlogeon1@gmail.com>
2018-01-29 06:21:45 +0700

    Fix migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,10 @@
 const FinTabToken = artifacts.require("FinTabToken.sol");
 const TokenBurner = artifacts.require("TokenBurner.sol");
+const ManualPriceProvider = artifacts.require("ManualPriceProvider.sol");
 
 module.exports = async (deployer, accounts) => {
     await deployer.deploy(FinTabToken);
     await deployer.deploy(TokenBurner, FinTabToken.address, accounts[0]);
+    await deployer.deploy(ManualPriceProvider, TokenBurner.address);
+    TokenBurner.setPriceProvider(ManualPriceProvider.address);
 };
