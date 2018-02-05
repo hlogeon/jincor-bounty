0a243cd011500bcd6c07f4571a3b8e6667ef9f3c
Andrey Degtyaruk <hlogeon1@gmail.com>
2018-02-05 19:21:18 +0700

    Return FinTabToken

---
+++ contracts/FinTabToken.sol
@@ -0,0 +1,136 @@
+pragma solidity ^0.4.15;
+
+import "./token/SmartToken.sol";
+import "./abstract/ContractReceiver.sol";
+
+/**
+ * @title FinTabToken
+ *
+ * @dev Burnable Ownable ERC223(and ERC20-compilant) token with support of
+ * Bancor SmartToken protocol
+ */
+contract FinTabToken is SmartToken {
+
+  uint public constant INITIAL_SUPPLY = 5000000 * (10 ** 8);
+
+  uint public releaseTokensBlock; //Approximatly will be at 01.07.2018
+
+  mapping (address => bool) public teamAddresses;
+  mapping (address => bool) public tokenBurners;
+
+  event Transfer(address indexed _from, address indexed _to, uint _value, bytes _data);
+
+  // Limit token transfer for  the team
+  modifier canTransfer(address _sender) {
+    require(block.number >= releaseTokensBlock || !teamAddresses[_sender]);
+    _;
+  }
+
+  modifier canBurnTokens(address _sender) {
+    require(tokenBurners[_sender] == true || owner == _sender);
+    _;
+  }
+
+  // Token construcor
+  function FinTabToken(uint _releaseBlock) public {
+    releaseTokensBlock = _releaseBlock;
+    totalSupply_ = INITIAL_SUPPLY;
+    balances[msg.sender] = INITIAL_SUPPLY;
+    NewSmartToken(this);
+  }
+
+  function name() public constant returns (string) { return "FinTab"; }
+  function symbol() public constant returns (string) { return "FNTB" ;}
+  function decimals() public constant returns (uint8) { return 8; }
+
+  function totalSupply() public constant returns (uint) {
+    return totalSupply_;
+  }
+  function balanceOf(address _owner) public constant returns (uint balance) {
+    require(_owner != 0x0);
+    return balances[_owner];
+  }
+
+
+  // Owner can allow a particular address (a crowdsale contract) to transfer tokens despite the lock up period.
+  function setTeamAddress(address addr, bool state) onlyOwner public {
+    require(addr != 0x0);
+    teamAddresses[addr] = state;
+  }
+
+  function setBurner(address addr, bool state) onlyOwner public {
+    require(addr != 0x0);
+    tokenBurners[addr] = state;
+  }
+
+  // Function that is called when a user or another contract wants to transfer funds .
+  function transfer(address _to, uint _value, bytes _data) transfersAllowed canTransfer(msg.sender) public returns (bool success) {
+    if(isContract(_to)) {
+        return transferToContract(_to, _value, _data);
+    }
+    else {
+        return transferToAddress(_to, _value, _data);
+    }
+  }
+
+  // Standard function transfer similar to ERC20 transfer with no _data .
+  // Added due to backwards compatibility reasons .
+  function transfer(address _to, uint _value) transfersAllowed canTransfer(msg.sender) public returns (bool success) {
+    //standard function transfer similar to ERC20 transfer with no _data
+    //added due to backwards compatibility reasons
+    bytes memory empty;
+    if(isContract(_to)) {
+        return transferToContract(_to, _value, empty);
+    }
+    else {
+        return transferToAddress(_to, _value, empty);
+    }
+  }
+
+  // Transfer on _from behalf
+  function transferFrom(address _from, address _to, uint _value) transfersAllowed canTransfer(_from) public returns (bool success) {
+    // Call Burnable.transferForm()
+    return super.transferFrom(_from, _to, _value);
+  }
+
+  // Burn tokens
+  function burn(uint _value) canBurnTokens(msg.sender) public returns (bool success) {
+    return super.burn(_value);
+  }
+
+  // Burn tokens on _from behalf
+  function burnFrom(address _from, uint _value) onlyOwner  canBurnTokens(msg.sender) public returns (bool success) {
+    return super.burnFrom(_from, _value);
+  }
+
+
+  //assemble the given address bytecode. If bytecode exists then the _addr is a contract.
+  function isContract(address _addr) private returns (bool is_contract) {
+      uint length;
+      assembly {
+            //retrieve the size of the code on target address, this needs assembly
+            length := extcodesize(_addr)
+      }
+      return (length>0);
+    }
+
+  //function that is called when transaction target is an address
+  function transferToAddress(address _to, uint _value, bytes _data) private canTransfer(msg.sender) returns (bool success) {
+    require(balanceOf(msg.sender) >= _value);
+    balances[msg.sender] = safeSub(balanceOf(msg.sender), _value);
+    balances[_to] = safeAdd(balanceOf(_to), _value);
+    Transfer(msg.sender, _to, _value, _data);
+    return true;
+  }
+
+  //function that is called when transaction target is a contract
+  function transferToContract(address _to, uint _value, bytes _data) private canTransfer(msg.sender) returns (bool success) {
+    require(balanceOf(msg.sender) >= _value);
+    balances[msg.sender] = safeSub(balanceOf(msg.sender), _value);
+    balances[_to] = safeAdd(balanceOf(_to), _value);
+    ContractReceiver receiver = ContractReceiver(_to);
+    receiver.tokenFallback(msg.sender, _value, _data);
+    Transfer(msg.sender, _to, _value, _data);
+    return true;
+  }
+}
--- transferBalances.js
+++ transferBalances.js
@@ -12,8 +12,21 @@ const config = {
   privateKey: '0x3244d69a1f78c29dfe094bdca9fab39cb18b3bae6307020e840089b4a38bedfe',
   gas: '50000',
   gasPrice: '10',
-  fromBlock: 0
+  fromBlock: 2289300
 };
+
+const duplicateTransactions = [
+// '0x9ea908cd5c77e8c1800392b585b6480920f57a05ed4a29842b900d7f14d8abd5', '0xb92690dc9131ee7ea4214c4f10e365461f0b1621755eb27f4345eeffe3346749',
+// '0x8ac562f4b118815616e4942b2d1c97b5042e461aa8dcca72216b313745ef6d8e', '0xa4302306cb973ba92c9b5da0dceb6779408309ba112ea83c0873ea25b06c4882',
+// '0xc37c7e748e48e80c1461e3712a2491f71227df28fd38b11ab888589e66bae175','0xafd8d44bf36badb9f76846fb7a1fa8df810d532f520681df0c0582947378bcdf',
+// '0x62c240a004d47750d80b281bad67d2fbde7c55db0d1f2bc82b3af1b458611a39','0xea046e4228caba851b539613c88535b1665c9a9ad1c5b6a0f1bcaf9ba805f12d',
+// '0x888f0e35ca33466be6de1406bea8bbbeb13286e7c4b695233331b2519e646c0e','0x7928280373f141604f5533caf99b8bb04d6fe57cf67fc16c50b5b349a47a0f61',
+// '0xd3d7b23d9d9340464de7aa55a40808f6c34e0fae54f6ae26cea5e00a864790c7','0x0680677b18c895201b28447020dd71ba28c97dd4b77adef5b4d86036306266e8'
+// '0x611f883560a2c583af984d028f4ca9f2cb0cd3390970f8740939cb564246936e'
+'0x4e05c2b507266a40036c56d48427bca4a47897425959e646c99d10d2bb8a5e51',
+'0x32c506cf0ae78fb5cb36953b670aff5373fe4a9a309d093786a42cbf7a1b6a1a',
+'0xc394b3e475e2d4f9b4cc0fcbbca1ff0b4fe802afed64ca88ffda19ccc7c13fab'
+];
 
 if (config.oldContractAddress == config.newContractAddress) {
   throw new Error('Contracts addresses are equals');
@@ -31,7 +44,12 @@ oldContract.getPastEvents('Transfer', config.fromBlock).then((eventResult) => {
   const BN = web3.utils.BN;
 
   for(e of eventResult) {
+    console.log("tx: ", e.transactionHash);
     const ev = e.returnValues;
+    if(duplicateTransactions.indexOf(e.transactionHash) !== -1) {
+      console.log("Duplicate tx: ", duplicateTransactions[duplicateTransactions.indexOf(e.transactionHash)]);
+      continue;
+    }
     if (typeof balances[ev._from] === 'undefined') {
       balances[ev._from] = new BN();
     }
